ec0f31addf11455a78d24836750a7ba2912af197
Keith Bennett <keithrbennett@gmail.com>
2011-11-29 16:13:03 -0500

    Added next_column_rownums method.

--- lib/fleuve/area.rb
+++ lib/fleuve/area.rb
@@ -2,8 +2,10 @@ module Fleuve
 
   class Area
 
-    attr_accessor :matrix_data
+    attr_accessor :matrix_data, :solution_array, :working_array
 
+
+    # Can be passed either a 2D array or a multiline string of space delimited numbers.
     def initialize(array2d_or_string)
       case array2d_or_string
         when Array
@@ -14,6 +16,9 @@ module Fleuve
     end
 
 
+    # Although the problem description says only "delimited integers",
+    # the input examples are all space delimited, so we'll make the
+    # assumption for now that these numbers will always be space delimited.
     def parse(multiline_rows_string)
       row_strings = multiline_rows_string.split("\n")
       rows_as_arrays = []
@@ -32,6 +37,21 @@ module Fleuve
 
     def column_count
       matrix_data[0].size
+    end
+
+
+    def next_column_rownums(current_rownum)
+      [
+        current_rownum - 1,  # -1 is ok
+        current_rownum,
+        (current_rownum == row_count - 1) ? 0 : current_rownum + 1
+      ]
+    end
+
+    def optimal_path(current_column_number, current_row_number)
+      (current_column_number...column_count).each do |column_number|
+
+      end
     end
   end
 end
--- spec/fleuve/area_spec.rb
+++ spec/fleuve/area_spec.rb
@@ -7,8 +7,10 @@ module Fleuve
 
 describe Area do
 
-  def array_data;  [[0, -1], [2, 4], [12, 18]]; end
-  def string_data; "0 -1 \n 2 4\n  12  18 ";     end
+  def array_data;      [[0, -1], [2, 4], [12, 18]];      end
+  def string_data;     "0 -1 \n 2 4\n  12  18 ";         end
+  def big_array_data;  ([string_data] * 3).join("\n");   end
+  def big_array_size;  9;                                end
 
   it "should instantiate with a two dimensional array" do
     Area.new(array_data).should be
@@ -35,7 +37,21 @@ describe Area do
     Area.new(string_data).column_count.should == 2
   end
 
+  it "should calculate the 3 possible row numbers correctly for non-edge rows" do
+    Area.new(big_array_data).next_column_rownums(3).should == [2, 3, 4]
+  end
 
+  it "should calculate the 3 possible row numbers correctly for bottom row" do
+    Area.new(big_array_data).next_column_rownums(big_array_size - 1).should == [big_array_size - 2, big_array_size - 1, 0]
+  end
+
+  it "should calculate the 3 possible row numbers correctly for the top row" do
+    Area.new(big_array_data).next_column_rownums(0).should == [-1, 0, 1]
+  end
+
+  it "should not permit initializing with invalid data" do
+    pending "Need to add validation of matrix data input."
+  end
 end
 
 end
