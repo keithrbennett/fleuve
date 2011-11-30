1e4d5ae35662d56327d68eb1b0c8d787a2d743c8
Keith Bennett <keithrbennett@gmail.com>
2011-11-29 20:15:01 -0500

    Worked on recursive calculation.  Added methods: resistance, eat_next_column, reached_last_column?, total_resistance, <=>, pop.

--- lib/fleuve/area.rb
+++ lib/fleuve/area.rb
@@ -2,7 +2,7 @@ module Fleuve
 
   class Area
 
-    attr_accessor :matrix_data, :solution_array, :working_array
+    attr_accessor :matrix_data
 
 
     # Can be passed either a 2D array or a multiline string of space delimited numbers.
@@ -39,6 +39,10 @@ module Fleuve
       matrix_data[0].size
     end
 
+    def resistance(row, col)
+      matrix_data[row][col]
+    end
+
 
     def next_column_rownums(current_rownum)
       [
@@ -48,10 +52,33 @@ module Fleuve
       ]
     end
 
-    def calc_optimal_path(current_column_number, current_row_number)
-      (current_column_number...column_count).each do |column_number|
 
+    def optimal_path
+      solution_path = nil
+      (0...row_count).each do |rownum|
+        working_path = new_path << rownum
+        row_optimal_path = calc_optimal_path(working_path)
+        if solution_path.nil? || (row_optimal_path < solution_path)
+          solution_path = row_optimal_path
+        end
       end
+      solution_path
+    end
+
+    # Here's the heart of the calculation.  This function is called recursively,
+    # once per column.
+    def calc_optimal_path(working_path, solution_path = nil)
+      if working_path.reached_last_column?
+        if working_path < solution_path
+          solution_path = working_path
+        end
+        next_column_rownums(working_path.last_column_row).each do |rownum|
+          working_path.eat_next_column(rownum)
+          calc_optimal_path(working_path, solution_path)
+        end
+        working_path.pop
+      end
+      solution_path
     end
 
     
@@ -62,22 +89,48 @@ module Fleuve
 
     class Path
 
-      attr_accessor :area, :rownums, :total_resistance
+      attr_accessor :area, :rownums
 
       def initialize(area)
         @area = area
         @rownums = []
-        @total_resistance = 0
       end
 
       def current_column
         rownums.size - 1
       end
 
+      def eat_next_column(rownum)
+        self << area.resistance(rownum, current_column + 1)
+      end
+
+      def reached_last_column?
+        current_column == area.column_count - 1 # || over maximum
+      end
+
+      # Yes, it's CPU-wasteful not to cache this value, but it can be optimized later.
+      def total_resistance
+        (0..current_column).to_a.inject(0) do |sum, colnum|
+          sum += area.resistance(rownums[colnum], colnum)
+        end
+      end
+
+      def last_column_row
+        rownums[-1]
+      end
+
       def <<(rownum)
         @rownums << rownum
-        resistance = area.matrix_data[rownum][current_column]
-        @total_resistance += resistance
+        self
+      end
+
+      def <=>(other)
+        total_resistance - other.total_resistance
+      end
+
+      # For stepping back a column.
+      def pop
+        rownums.pop
         self
       end
     end
